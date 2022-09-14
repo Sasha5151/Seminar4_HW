7384405f9141a4a3b0fba2142fbcf6911d9286e9
Sasha <F712@rambler.ru>
2022-09-14 13:02:58 +0300

    29

--- Program.cs
+++ Program.cs
@@ -51,3 +51,23 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+int size = 8 ;
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+    {   
+        for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0,100);
+    }
+
+void PrintArray(int[] array)
+    {   
+        System.Console.Write("[ ");
+        for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+        System.Console.Write("]");
+    }
+
+//End
